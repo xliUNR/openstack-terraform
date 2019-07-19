c199bdd96699121d2f4c02bd911b0db95c103d53
Xli <xli@nevada.unr.edu>
2019-07-19 12:15:42 -0700

    started main terraform file and example file for variables

---
+++ main.tf
@@ -0,0 +1,49 @@
+#  Setup input variables, don't want secrets in .tf
+variable "oRegion" {
+  description = "Openstack region"
+  default = "regionOne"
+}
+
+variable "authURL" {
+  description = "Authentication url"
+}
+
+variable "userName" {
+  description = "user name for administrator account"
+  default = "admin"
+}
+
+variable "tenantName" {
+  description = "openstack tenant name"
+  default = "admin"
+}
+
+variable "adminPass" {
+  description = "password for admin credentials"
+}
+
+# Configure openstack provider
+provider "openstack" {
+  user_name = "${var.userName}"
+  tenant_name = "${var.tenantName}"
+  auth_url = "${var.authURL}"
+  password = "${var.adminPass}"
+  region = "${var.oRegion}"
+  region = "regionOne"
+}
+
+# create network
+resource "openstack_networking_network_v2" "network_1" {
+  name = "network_1"
+  admin_state_up = "true"
+}
+
+# create test subnet
+resource "openstack_networking_subnet_v2" "subnet1" {
+  name = "subnet_1"
+  network_id = ""
+  cidr = ""
+  ip_version = 4
+  
+}
+
