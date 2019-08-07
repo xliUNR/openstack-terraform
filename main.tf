8fa90578f76cf1ae2f843c9f30e221127228230f
xli <jakewheeler@unr.edu>
2019-08-07 19:25:05 +0000

    Fixed DNS issue, changed nameserver IPs

--- main.tf
+++ main.tf
@@ -41,10 +41,10 @@ resource "openstack_networking_network_v2" "provider" {
 resource "openstack_networking_subnet_v2" "external_subnet_1" {
   name            = "external_subnet_1"
   cidr            = "134.197.20.175/24"
-  #dns_nameservers = ["8.8.8.8"]
-  dns_nameservers = ["127.0.0.53"]
+  #dns_nameservers = ["127.0.0.53"]
+  dns_nameservers = ["134.197.5.1", "134.197.6.1"]
   network_id      = "${openstack_networking_network_v2.provider.id}"	
-  enable_dhcp = "false" #For some reason dhcp breaks DNS service on controller node so have to disable
+  enable_dhcp = "true" 
   allocation_pool {
     start = "134.197.20.190" 
     end   = "134.197.20.195"
@@ -64,14 +64,15 @@ resource "openstack_networking_subnet_v2" "subnet1" {
   network_id = "${openstack_networking_network_v2.internalNet_1.id}"
   cidr = "192.168.1.20/24" # This choice is arbitrary
   ip_version = 4
-  dns_nameservers = ["127.0.0.53"]
+  dns_nameservers = ["134.197.5.1", "134.197.6.1"]
+  #dns_nameservers = ["127.0.0.53"]
   
 }
 
 # create a virtual router
 resource "openstack_networking_router_v2" "router_1" {
   name = "router_1"
-  #external_network_id = "${openstack_networking_network_v2.provider.id}"
+  external_network_id = "${openstack_networking_network_v2.provider.id}"
 }
 
 /*# create external router
@@ -81,7 +82,7 @@ resource "openstack_networking_router_v2" "extrouter" {
 }
 */
 # create an interface between router and internal net
-resource "openstack_networking_router_interface_v2" "internal_router" {
+resource "openstack_networking_router_interface_v2" "subnet1_if" {
   router_id = "${openstack_networking_router_v2.router_1.id}"
   subnet_id = "${openstack_networking_subnet_v2.subnet1.id}"  
 }
