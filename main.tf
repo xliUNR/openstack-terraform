4ff46912f3812f25ee20c984e9ba66f70b730476
Xli <xli@nevada.unr.edu>
2019-08-12 11:52:58 -0700

    made new main file for root module and declared variables in provider

---
+++ main.tf
@@ -0,0 +1,13 @@
+# This is the main file for the root node. This will call other modules
+
+# Create network using network module
+module "neutron" {
+  source = "modules/neutron"
+  # Now here you can add variables used in the modules
+  # But for now it is all hardcoded.
+}
+
+module "glance" {
+  source = "modules/glance"
+  
+}
--- provider.tf
+++ provider.tf
@@ -4,6 +4,25 @@ variable "oRegion" {
   default     = "RegionOne"
 }
 
+variable "authURL" {
+  description = "Authentication URL"
+}
+
+variable "userName" {
+  description = "user name for administrator account"
+  default     = "admin"
+}
+
+variable "tenantName" {
+  description = "openstack tenant name"
+  default     = "admin"
+}
+
+variable "adminPass" {
+  description = "password for admin credentials"
+}
+
+
 # Configure openstack provider with variables from a .tfvars file. 
 # This is so that we do not upload secrets onto Github.
 provider "openstack" {
