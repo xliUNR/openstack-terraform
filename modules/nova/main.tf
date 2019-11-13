6d1013cb24da50f926cbbb3879bde605b5ec95d4
Xli <xli@nevada.unr.edu>
2019-11-13 10:15:01 -0800

    added cirrOS instance to nova

--- modules/nova/main.tf
+++ modules/nova/main.tf
@@ -1,19 +1,29 @@
 # This contains the configuration for nova (compute)
 
 # First make a key pair
-resource "openstack_compute_keypair_v2" "test-keypair" {
+/*resource "openstack_compute_keypair_v2" "test-keypair" {
   name = "test-keypair"
-}
+}*/
 
-
-resource "openstack_compute_instance_v2" "winServer" {
-  name = "winServer"
+# Windows test image
+resource "openstack_compute_instance_v2" "winSTest" {
+  name = "winSTest"
   image_name  = "windowsServer2012"
-  flavor_name = "windows_4gb_8cpu_40gb"
-  key_pair = openstack_compute_keypair_v2.test-keypair.name
+  flavor_name = "winTest"
+  #key_pair = openstack_compute_keypair_v2.test-keypair.name
   security_groups = ["default"]
   network {
     name = "internalNet_1"
   }
 }
-
+# CirrOS test image
+resource "openstack" "cirrTest" {
+  name = "cirrTest"
+  image_name = "cirrOS"
+  flavor_name = "smallTest"
+  security_groups = ["default"]
+  network{
+    name = "internalNet_1"
+  }
+}
+resource "openstack_compute_instance_v2" "cirrOSTest"
